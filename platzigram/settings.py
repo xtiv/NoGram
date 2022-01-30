5506d232f41ae42563dcea86b0d468c289409dff
xtiv <xtiv@mail.com>
2022-01-30 19:12:02 +0100

    funcional

--- platzigram/settings.py
+++ platzigram/settings.py
@@ -80,8 +80,12 @@ WSGI_APPLICATION = 'platzigram.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'platzi',
+        'USER': 'freddier',
+        'PASSWORD': 'cvander<3',
+        'HOST': 'localhost',
+        'PORT': '5432'
     }
 }
 
